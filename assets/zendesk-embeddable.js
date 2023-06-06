28b2ac31da4ce10b3a9675187b330fe66bb76aae
kamalkj9460 <79216365+kamalkj9460@users.noreply.github.com>
2023-06-06 23:08:32 +0530

    Before OPT
    Before OPT

---
+++ assets/zendesk-embeddable.js
@@ -0,0 +1,1 @@
+var script = document.createElement('script');script.type = 'text/javascript';script.id = 'ze-snippet';script.src = 'https://static.zdassets.com/ekr/snippet.js?key=576bc6af-9046-461b-b473-f9ce41fa9b1c';document.getElementsByTagName('head')[0].appendChild(script);
